1e6c5914f8f3618024094c70d354d52f57aba8b0
Mayank Gupta <mayankgupta270606@gmail.com>
2026-06-28 17:43:01 +0530

    refractor to render deploy for backend

--- Server/app.js
+++ Server/app.js
@@ -7,24 +7,25 @@ const taskRoutes = require("./routes/taskRoutes");
 
 dotenv.config();
 
-const app = express();
-
-// Connect database
 connectDB();
 
-// Middleware
+const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
-// Routes
 app.use("/api/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
-  res.json({
-    success: true,
-    message: "Task Tracker API Running",
-  });
+    res.json({
+        success: true,
+        message: "Task Tracker API Running"
+    });
 });
 
-// Export app for Vercel
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,21 +1,21 @@
 import api from "../api/axios";
 
 export const getTasks = async () => {
-  const response = await api.get("api/tasks");
+  const response = await api.get("/tasks");
   return response.data;
 };
 
 export const createTask = async (task) => {
-  const response = await api.post("api/tasks", task);
+  const response = await api.post("/tasks", task);
   return response.data;
 };
 
 export const updateTask = async (id, task) => {
-  const response = await api.put(`api/tasks/${id}`, task);
+  const response = await api.put(`/tasks/${id}`, task);
   return response.data;
 };
 
 export const deleteTask = async (id) => {
-  const response = await api.delete(`api/tasks/${id}`);
+  const response = await api.delete(`/tasks/${id}`);
   return response.data;
 };
